e9cb22717836d4d7fa48341df434e4d11a03a2f9
Diego Naranjo <diegonaranjo@Diegos-MacBook-Pro.local>
2022-12-17 10:32:03 -0500

    18 - object keys, values, entries

---
+++ clase2/18.js
@@ -0,0 +1,11 @@
+const persona = {
+  name: "Mate",
+  lastname: "Naran",
+  edad: 3,
+  country: 'Col'
+}
+
+console.log(persona)
+console.log(Object.keys(persona))
+console.log(Object.values(persona))
+console.log(Object.entries(persona))
